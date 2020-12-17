3b84a821611929cab9a92dc7100b79e7fc21ecc4
Eitan Mosenkis <eitan@mosenkis.net>
2020-12-17 23:10:51 +0200

    Support preserving commit history in the target repo.
    Also adds double quotes in a few places for extra safety.

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,6 +32,10 @@ if [[ -z "$BUILD_THEMES" ]]; then
     BUILD_THEMES=true
 fi
 
+if [[ -z "$PRESERVE_HISTORY" ]]; then
+    PRESERVE_HISTORY=false
+fi
+
 if [[ -z "$GITHUB_TOKEN" ]] && [[ "$BUILD_ONLY" == false ]]; then
     echo "Set the GITHUB_TOKEN env variable."
     exit 1
@@ -49,12 +53,12 @@ main() {
 
     version=$(zola --version)
     remote_repo="https://${GITHUB_TOKEN}@github.com/${TARGET_REPOSITORY}.git"
-    remote_branch=$PAGES_BRANCH
+    remote_branch="$PAGES_BRANCH"
 
     echo "Using $version"
 
     echo "Building in $BUILD_DIR directory"
-    cd $BUILD_DIR
+    cd "$BUILD_DIR"
 
     echo Building with flags: ${BUILD_FLAGS:+"$BUILD_FLAGS"}
     zola build ${BUILD_FLAGS:+$BUILD_FLAGS}
@@ -66,13 +70,24 @@ main() {
         echo "Pushing artifacts to ${TARGET_REPOSITORY}:$remote_branch"
 
         cd public
-        git init
+        if [[ "$PRESERVE_HISTORY" ]]; then
+            git clone -b "${remote_branch}" --depth 1 --no-checkout --separate-git-dir .git "${remote_repo}" "$(mktemp -d)"
+        else
+            git init
+            git checkout -b "${remote_branch}"
+        fi
         git config user.name "GitHub Actions"
         git config user.email "github-actions-bot@users.noreply.github.com"
         git add .
 
-        git commit -m "Deploy ${TARGET_REPOSITORY} to ${TARGET_REPOSITORY}:$remote_branch"
-        git push --force "${remote_repo}" master:${remote_branch}
+        # Only try to commit if there are changes (useful when PRESERVE_HISTORY is true).
+        git diff-index --cached --quiet HEAD || \
+            git commit -m "Deploy ${TARGET_REPOSITORY} to ${TARGET_REPOSITORY}:$remote_branch"
+        if [[ "$PRESERVE_HISTORY" ]]; then
+            git push "${remote_repo}"
+        else
+            git push --force "${remote_repo}" "master:${remote_branch}"
+        fi
 
         echo "Deploy complete"
     fi
