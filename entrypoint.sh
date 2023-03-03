351d3288d4f9eb1d277d3f71eae7206977d62a39
James Liu <contact@jamessliu.com>
2023-03-03 03:35:09 -0800

    Test other globs

--- entrypoint.sh
+++ entrypoint.sh
@@ -63,8 +63,8 @@ main() {
     # needed or else we get 'doubious ...' error
     echo "Set safe directories"
     git config --global --add safe.directory /github/workspace
-    git config --global --add safe.directory /github/workspace/sass/...
-    git config --global --add safe.directory /github/workspace/themes/...
+    git config --global --add safe.directory /github/workspace/sass/**/*
+    git config --global --add safe.directory /github/workspace/themes/*
 
     if ${BUILD_THEMES}; then
         echo "Fetching themes"
